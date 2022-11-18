17b8c5be06f7ed6ca3bcc9ab477aad507a0f821d
cagri <mehmetcagrikaratas@gmail.com>
2022-11-18 15:17:35 +0300

    added admin model and migration

---
+++ database/migrations/2022_11_18_104855_create_admins_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('name');
+            $table->string('type');
+            $table->string('mobile');
+            $table->string('image');
+            $table->integer('vendor_id');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('status')->default(1);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
