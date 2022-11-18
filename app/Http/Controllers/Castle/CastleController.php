66e4b793121fbd17443d98475e917a209d5b9ab0
cagri <mehmetcagrikaratas@gmail.com>
2022-11-18 15:17:59 +0300

    added castle controller

--- app/Http/Controllers/Castle/CastleController.php
+++ app/Http/Controllers/Castle/CastleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Castle;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
 
 class CastleController extends Controller
 {
@@ -17,6 +18,16 @@ class CastleController extends Controller
         return view('castle.castle_login');
     }
 
+    public function loginPost(Request $request)
+    {
+        $check = $request->all();
+        if (Auth::guard('admin')->attempt(['email' => $check['email'],
+            'password' => $check['password'], 'status' => 1])) {
+            return redirect()->route('castle.dashboard')->with('success', 'Login Success!');
+        } else {
+            return back()->with('error', 'Wrong Email or password!');
+        }
 
+    }
 
 }
