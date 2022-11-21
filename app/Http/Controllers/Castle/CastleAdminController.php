fbd3ffd1200f3d99c39fb1c28c00ce811c1a34a9
cagri <mehmetcagrikaratas@gmail.com>
2022-11-21 09:17:21 +0300

    added admin controller and blades

---
+++ app/Http/Controllers/Castle/CastleAdminController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Castle;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
+
+class CastleAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        $admins = Admin::all();
+        return view('castle.admin.index',compact('admins'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create()
+    {
+        return view('castle.admin.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        Admin::create([
+            'uuid' => Str::uuid(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'type' => $request->type,
+            'mobile' => $request->mobile,
+            'image' => $request->image,
+            'vendor_id' => $request->vendor_id,
+        ]);
+        return redirect()->route('castle.admin.index')->with('success', 'Success Created!');
+
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function edit($id)
+    {
+        $adminEdit = Admin::find($id);
+        return view('castle.admin.edit',compact('adminEdit'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return RedirectResponse
+     */
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $adminId = Admin::findOrFail($request->id);
+        $adminId->uuid = Str::uuid();
+        $adminId->name = $request->name;
+        $adminId->email = $request->email;
+        $adminId->password = Hash::make($request->password);
+        $adminId->type = $request->type;
+        $adminId->mobile = $request->mobile;
+        $adminId->image = $request->image;
+        $adminId->status = $request->status;
+        $adminId->update();
+
+        return redirect()->route('castle.admin.index')->with('success', 'Success Updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $adminDelete = Admin::find($id);
+        $adminDelete->delete();
+        return redirect()->route('castle.admin.index')->with('success', 'Success Deleted!');
+    }
+}
---
+++ resources/views/castle/admin/create.blade.php
@@ -0,0 +1,75 @@
+@extends('castle.castle_master')
+@section('title','Dil Düzenle')
+@section('castle')
+
+    <main class="page-content">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">@yield('title')</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!--end breadcrumb-->
+
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <div class="card">
+                    <div class="card-header py-3 bg-transparent">
+                        <h5 class="mb-0">@yield('title')</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="border p-3 rounded">
+                            <form class="row g-3" action="{{route('castle.admin.store')}}" method="post">
+                                @csrf
+                                <div class="col-12">
+                                    <label class="form-label">Name</label>
+                                    <input type="text" name="name"  class="form-control" placeholder="Name">
+                                    @error("name")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Email</label>
+                                    <input type="email" name="email"  class="form-control" placeholder="Email">
+                                    @error("email")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Password</label>
+                                    <input type="password" name="password"  class="form-control" placeholder="*******">
+                                    @error("password")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Type</label>
+                                    <input type="password" name="type"  class="form-control" placeholder="*******">
+                                    @error("password")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary px-4">Add</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--end row-->
+
+    </main>
+
+@endsection
