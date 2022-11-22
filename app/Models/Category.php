991b58ffa2b6564916499f500137063688fea294
cagri <mehmetcagrikaratas@gmail.com>
2022-11-22 09:13:18 +0300

    added slug and relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory,Sluggable;
     protected $fillable = [
         'name',
         'uuid',
@@ -15,4 +17,31 @@ class Category extends Model
         'slug',
         'status'
     ];
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function sluggableEvent(): string
+    {
+        /**
+         * Default behaviour -- generate slug before model is saved.
+         */
+        return SluggableObserver::SAVING;
+
+        /**
+         * Optional behaviour -- generate slug after model is saved.
+         * This will likely become the new default in the next major release.
+         */
+        return SluggableObserver::SAVED;
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
 }
