9edd2306fd33bef4e7b021591fb847343cfabf15
cagri <mehmetcagrikaratas@gmail.com>
2022-11-25 13:25:58 +0300

    site template extends

---
+++ resources/views/site/site_master.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="zxx">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Links of CSS files -->
+    <link rel="stylesheet" href="{{asset('assets/site/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/boxicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/magnific-popup.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/nice-select.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/slick.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/meanmenu.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/rangeSlider.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/dark.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/site/css/responsive.css')}}">
+
+    <title>Xton - eCommerce HTML Template</title>
+
+    <link rel="icon" type="image/png" href="{{asset('assets/site/img/favicon.png')}}">
+</head>
+<body>
+
+<!-- Start Top Header Area -->
+@include('site.layouts.header')
+<!-- End Top Header Area -->
+
+@include('site.layouts.navbar')
+
+@yield('site')
+
+<!-- Start Footer Area -->
+@include('site.layouts.footer')
+<!-- End Footer Area -->
+
+<div class="go-top"><i class='bx bx-up-arrow-alt'></i></div>
+
+<!-- Links of JS files -->
+<script src="{{asset('assets/site/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/site/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/site/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/site/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('assets/site/js/magnific-popup.min.js')}}"></script>
+<script src="{{asset('assets/site/js/parallax.min.js')}}"></script>
+<script src="{{asset('assets/site/js/rangeSlider.min.js')}}"></script>
+<script src="{{asset('assets/site/js/nice-select.min.js')}}"></script>
+<script src="{{asset('assets/site/js/meanmenu.min.js')}}"></script>
+<script src="{{asset('assets/site/js/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('assets/site/js/slick.min.js')}}"></script>
+<script src="{{asset('assets/site/js/sticky-sidebar.min.js')}}"></script>
+<script src="{{asset('assets/site/js/wow.min.js')}}"></script>
+<script src="{{asset('assets/site/js/form-validator.min.js')}}"></script>
+<script src="{{asset('assets/site/js/contact-form-script.js')}}"></script>
+<script src="{{asset('assets/site/js/ajaxchimp.min.js')}}"></script>
+<script src="{{asset('assets/site/js/main.js')}}"></script>
+</body>
+</html>
